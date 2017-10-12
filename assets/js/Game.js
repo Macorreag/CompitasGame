7d7cc514afb738de2354a6c77c6d77b173e899a1
Miller Correa <macorreag@unal.edu.co>
2017-10-12 00:57:07 -0500

    Se pasaron a obetos las ball faltan metodos y tiene bugs

--- assets/js/Game.js
+++ assets/js/Game.js
@@ -1,29 +1,106 @@
+/*|||||||||||||||||||||||||||||||||||||||||||||||||||||*/
+/*|||||||||||||||||||||||||||||||||||||||||||||||||||||*/
+/*                  PUNTAJES                           */
+/* *compita1 = -1 punto                                */
+/* *policia1 = +1 punto                                */
+/* *estudiante1 = +2 puntos                            */
+/*|||||||||||||||||||||||||||||||||||||||||||||||||||||*/
+
+function borrarId ( id ){
+        var objeto = document.getElementById(id);
+    //var elemento = document.getElementById( "Cabeza");
+   // var ball = elemento;
+    /*Borra un elemento */
+            objeto.parentNode.removeChild(objeto);
+    //Compa1.parentNode.removeChild(Compa1);
+//alert('manejado desde JS');
+}  
+
+function Ball ( name ) {
 
         
+    this.id = name;
+    this.objeto = document.getElementById(name);
+    this.y = Math.floor((Math.random() * (this.objeto.offsetHeight-22)) + 1);
+    /*offsetHeight -> Extrae el alto de un elemento con padding y bordes*/
+    this.x = Math.floor((Math.random() * (this.objeto.offsetWidth-22)) + 1);
+    /*offsetHeight -> Extrae el alto de un elemento con padding y bordes*/
+    this.objeto.style.left=String(this.x)+"px";
+    /*Cambiamos la posicion de la pelota aletrando el CSS*/
+    this.objeto.style.top=String(this.y)+"px";
+        
+         
+    
+    
+    this.objeto.addEventListener("click", function() {
+            borrarId(this.id);
+            clearInterval(idSetInterval);
+           
+        /*falta parar el moviemiento de la pelota para que no genere errores*/
+        //borrarBall(Compa1);
+        //ballRed.delBall();
+         });
+    
+    
+    
+   
+       
+    
+   
+    
+  
+}
+
+
+//function borrarBall(elemento){
+    
+    //var elemento = document.getElementById( "Cabeza");
+   // var ball = elemento;
+    /*Borra un elemento */
+  //  elemento.parentNode.removeChild(elemento);
+    //Compa1.parentNode.removeChild(Compa1);
+//alert('manejado desde JS');
+    
+//}
+
+
+function IniciarGame(){
+    
+    var puntaje = 0;
+
+}
        
 window.onload = function()  /*funcion que se ejecuta al terminar la carga del documento*/
 
 {
+   
+   
 /*posicionamos la pelota de manera aleatoria en la pantalla*/
     
-    /*********************BUG!!! no es responsive*****************/
-    /*Pensar como posicionar nuevamente la imagen tras un cambio del tamaño de la ventana 
-    *Asumir mediadas relativas y calcular respecto estas el random 
-    *
+
+
+    var Compa1Cabeza = document.getElementById( "Cabeza");
+    var Compa1 = document.getElementById( "Compa1");
+    
+   
+    
+    var ballRed = new Ball ("Compa1");
+    //Compa1Cabeza.addEventListener("click", function() {
+      //  ballRed.delBall();
+        //borrarBall(Compa1);
+        //ballRed.delBall();
+//    });
+    
+    /*Compa1Cabeza.addEventListener("onclick", borrarBall("Cabeza"));
+    /*
+    document.getElementById("Cabeza").addEventListener("click", myFunction);
+
+function myFunction() {
+    document.getElementById("Cabeza").parentNode.removeChild(document.getElementById("Cabeza"));
+}
     */
-    var y = Math.floor((Math.random() * (document.getElementById("Montaje").offsetHeight-22)) + 1);
-    /*offsetHeight -> Extrae el alto de un elemento con padding y bordes*/
-    var x = Math.floor((Math.random() * (document.getElementById("Montaje").offsetWidth-22)) + 1);
-    /*offsetHeight -> Extrae el alto de un elemento con padding y bordes*/
-    document.getElementById("Compa1").style.left=String(x)+"px";
-    /*Cambiamos la posicion de la pelota aletrando el CSS*/
-    document.getElementById("Compa1").style.top=String(y)+"px";
-    
-  
-    
-    
-    
-    
+
+
 }
  
  
@@ -40,6 +117,16 @@ function moverElemento(){
          
      
 }   
+    
+
+    
+   /* Compa1Cabeza.onclick = function (){
+        
+        /*Borra un elemento */
+     /*   Compa1.parentNode.removeChild(Compa1);
+        
+        alert('manejado desde JS');
+    }*/
      
      /*var Compa1 = document.getElementById('Compa1');
                 Compa1.onclick = function () {
@@ -61,14 +148,15 @@ function moverElemento(){
          
 
 }
-       // controlY, determina dirección vertical: 1-baja|0-sube
-        var controlY=Math.floor((Math.random() * 1) + 0);
+/* controlY, determina dirección vertical: 1-baja|0-sube */
+var controlY=Math.floor((Math.random() * 1) + 0);
  
-        // controlX, determina dirección vertical: 1-derecha|0-izquierda
-        var controlX=Math.floor((Math.random() * 1) + 0);
+/* controlX, determina dirección vertical: 1-derecha|0-izquierda */
+var controlX=Math.floor((Math.random() * 1) + 0);
  
-        // definimos las variables que contienen la posicion de la pelota
-        var x=y=0;
+/* definimos las variables que contienen la posicion de la pelota */
+var x = 0;
+var y = 0;
  
         // definimos la velocidad de movimiento en pixels
         var velocidad=1;
